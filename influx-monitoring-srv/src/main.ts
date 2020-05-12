d762ae05d5e4be6917736fdce8c7218da1c87187
Max K <max@ezic.io>
2020-05-12 10:03:27 +0300

    Initialized srv app, added configuration module usage with .env file; added gitignore;

---
+++ influx-monitoring-srv/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {Logger} from "@nestjs/common";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.APP_PORT);
+  Logger.log(`Server is running on port ${process.env.APP_PORT}`, "SUCCESS!");
+}
+bootstrap();
